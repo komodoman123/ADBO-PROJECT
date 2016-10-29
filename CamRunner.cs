68d792e23751be6b2b2dc528d04a53e881efdbd1
BlueYamato <fraxinusratatoshkr@gmail.com>
2016-10-29 20:12:03 +0900

    Add files via upload

---
+++ CamRunner.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamRunner : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector3.right * PlayerPrefs.GetInt("speed") * Time.deltaTime);
+    }
+}
