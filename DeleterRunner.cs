09b5f748d16f0d084fd86b3ee8accf5e855b5e73
adamadamadamadamadam <adamnurmishwari@gmail.com>
2016-10-29 19:09:16 +0700

    Add files via upload

---
+++ DeleterRunner.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeleterRunner : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(Vector3.right * PlayerPrefs.GetInt("speed") * Time.deltaTime);
+	}
+
+	void OnTriggerEnter2D(Collider2D collider){
+		Destroy (((BoxCollider2D)collider).gameObject);
+	}
+}
--- Obstacle.cs
+++ Obstacle.cs
@@ -2,33 +2,40 @@
 using System.Collections;
 
 public class Obstacle : MonoBehaviour {
-    public GameObject[] ob;
-    public Transform pos;
+	public GameObject[] ob;
+	public Transform pos;
 	// Use this for initialization
 	void Start () {
-        ObstacleMaker();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        transform.Translate(Vector3.right * PlayerPrefs.GetInt("speed") * Time.deltaTime);
-       // GameObject cs = GameObject.Find("Quad");
-       // if (pos.position.x - cs.transform.position.x > 25)
-      //  {
-      //      Destroy(cs);          ini buat nge destroy obstacle yang udh lewat dari kamera
-      //  }                         cuman gk kebaca jadinya gw jadiin commend
+		ObstacleMaker();
 	}
 
-    //method buat ngespawn si obstacle
-    // ps: di unity bisa masukin ke dalem arraynya
-    // Quad adalah BoxCollider jadi nnt spawn dari quad dan bisa buat 2 Quad, 1 yang buat flyingQuad, 1 lagi yang ground Quad
-    void ObstacleMaker()
-    {
-        GameObject obs = (GameObject)Instantiate(ob[Random.Range(0, ob.Length)], transform.position, Quaternion.identity);
-        obs.name = "Quad";
-        obs.AddComponent<BoxCollider2D>();
-        obs.GetComponent<BoxCollider2D>().isTrigger = true;
-        float mark = Random.Range(1, 5);
-        Invoke("ObstacleMaker", mark);
-    }
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(Vector3.right * PlayerPrefs.GetInt("speed") * Time.deltaTime);
+		// Nanti di unity bikin lg gameobject baru. Scriptnya pake DeleterRunner
+		// Gameobjectnya ada di belah kiri kamera.
+		// Si gameobject barunya kasih component RigidBody2D sama BoxCollider2D
+		// Di RigidBody2Dnya si kinematicnya diok-in
+		// Harusnya waktu quad yg kespawnya kena, quadnya lgs kedestroy
+		/*
+			// GameObject cs = GameObject.Find("Quad");
+			// if (pos.position.x - cs.transform.position.x > 25)
+			//  {
+			//      Destroy(cs);          ini buat nge destroy obstacle yang udh lewat dari kamera
+			//  }                         cuman gk kebaca jadinya gw jadiin commend
+		*/
+	}
+
+	//method buat ngespawn si obstacle
+	// ps: di unity bisa masukin ke dalem arraynya
+	// Quad adalah BoxCollider jadi nnt spawn dari quad dan bisa buat 2 Quad, 1 yang buat flyingQuad, 1 lagi yang ground Quad
+	void ObstacleMaker()
+	{
+		GameObject obs = (GameObject)Instantiate(ob[Random.Range(0, ob.Length)], transform.position, Quaternion.identity);
+		obs.name = "Quad";
+		obs.AddComponent<BoxCollider2D>();
+		obs.GetComponent<BoxCollider2D>().isTrigger = false; // Diganti jd false
+		float mark = Random.Range(1, 5);
+		Invoke("ObstacleMaker", mark);
+	}
 }
