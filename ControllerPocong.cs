5c95f4bc6fa314b5669484a7f174805fac046183
BlueYamato <fraxinusratatoshkr@gmail.com>
2016-10-29 20:25:55 +0900

    Add files via upload

---
+++ ControllerPocong.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControllerPocong : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+    public bool falling;
+    public float jump;
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        PlayerPrefs.SetInt("speed", 5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.right * PlayerPrefs.GetInt("speed") * Time.deltaTime);
+        if (Input.GetKeyDown(KeyCode.UpArrow) == true && falling == false)
+        {
+            rb.velocity = new Vector3(0, jump);
+        }
+    }
+}
