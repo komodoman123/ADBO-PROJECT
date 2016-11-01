b9bf73299a3881f425018dc6f3e39167b2c1ef46
komodoman123 <rifqi546@gmail.com>
2016-11-01 21:57:49 +0700

    Add files via upload
    script score + camera

---
+++ Score - camRunner.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Score : MonoBehaviour {
+	public int sc;
+	public float sco;
+	public int high;
+	public GUIStyle style1;
+
+
+	void Start () {
+		sco = 0;
+		PlayerPrefs.SetInt ("high", 0);
+	}
+
+
+	void Update () {
+		sco += Time.deltaTime * 10;
+		sc = (int)sco;
+		if (high < sco) {
+			PlayerPrefs.SetInt ("high", sc);
+			transform.Translate(Vector3.right * PlayerPrefs.GetInt("speed") * Time.deltaTime);
+		}
+	}
+	
+		void OnGUI ()
+	{
+		string hi = high.ToString ();
+		string score = sc.ToString ();
+
+		GUI.Label (new Rect (Screen.width * 0.8f, Screen.height * 0.07f, Screen.width * 0.2f, Screen.height * 0.05f), score,style1);
+		GUI.Label (new Rect (Screen.width * 0.65f, Screen.height * 0.07f, Screen.width * 0.2f, Screen.height * 0.05f), "HI " + hi,style1);
+
+	}
+}
